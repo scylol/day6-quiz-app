78e9a2d53157ac9dc5d38dbe5a5ec5dc4df15f43
Scylol <djd3dobbin@gmail.com>
2017-04-10 11:25:58 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Create your initial state object
+var appState = {
+   questions: [
+   		{ 
+   			question: "what is your name?",
+   			choices: ['bob','2', '3', '4'],
+   			correctAnswer: 0;
+   			
+
+   		}
+
+
+   ],
+   score: 0
+   
+   		
+   	
+    // Answers
+    // User's answer choice(s)	
+    // Message(s) to let them know they have the correct answer	
+    // Message(s) when they don't have the correct answer
+    // Other things like score? Anything else?
+};
+
+console.log(appState.quiz[0].question);
+
+// State manipulation functions...
+// Render functions...
+
+// Event handlers
+// When start button is submitted
+$('.start').submit(function(event) {
+});
+
+// Current answer is submitted
+$('.answer').submit(function(event) {
+});
+
+// Next question
+$('.next').click(function(event) {
+});
+
+// Restart button is clicked
+$('.restart').click(function(event) {
+});
