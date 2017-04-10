e591b9114314b829a3d38fdc62d5bba388a33b7b
Scylol <djd3dobbin@gmail.com>
2017-04-10 12:39:57 -0400

    check answer function

--- app.js
+++ app.js
@@ -2,19 +2,47 @@
 var appState = {
    questions: [
    		{ 
-   			question: "what is your name?",
+   			question: "question1",
    			choices: ['bob','2', '3', '4'],
-   			correctAnswer: 0;
-   			
+   			correctAnswer: 0,
+        goodFeedback: 'Correct',
+        badFeedback: 'Incorrect, the correct answer was bob'
+   		},
+      
+      { 
+        question: "question2",
+        choices: ['bob','2', '3', '4'],
+        correctAnswer: 1,
+        goodFeedback: 'Correct',
+        badFeedback: 'Incorrect, the correct answer was 2'
+      },
 
-   		}
-
+      { 
+        question: "question3",
+        choices: ['bob','2', '3', '4'],
+        correctAnswer: 2,
+        goodFeedback: 'Correct',
+        badFeedback: 'Incorrect, the correct answer was 3'
+      },
+      
+      { 
+        question: "question4",
+        choices: ['bob','2', '3', '4'],
+        correctAnswer: 3,
+        goodFeedback: 'Correct',
+        badFeedback: 'Incorrect, the correct answer was 4'
+      },
+      
+      { 
+        question: "question5",
+        choices: ['bob','2', '3', '4'],
+        correctAnswer: 4,
+        goodFeedback: 'Correct',
+        badFeedback: 'Incorrect, the correct answer was bob'
+      }
 
    ],
    score: 0
-   
-   		
-   	
     // Answers
     // User's answer choice(s)	
     // Message(s) to let them know they have the correct answer	
@@ -23,6 +51,17 @@ var appState = {
 };
 
 console.log(appState.quiz[0].question);
+
+// State manipulation functions...
+// function to check user answer (if statement display fb)
+function checkAnswer(appState, userInput, currentQuestion) {
+  if (userInput === appState.questions[currentQuestion].correctAnswer){
+    appState.questions[currentQuestion].goodFeedback;
+    score++;
+  }
+  else {
+    appState.questions[currentQuestion].badFeedback;
+  }
 
 // State manipulation functions...
 // Render functions...
