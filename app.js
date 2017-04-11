87199cd5f39aa197f769ee975afbaa14236b36eb
Scylol <djd3dobbin@gmail.com>
2017-04-11 16:38:33 -0400

    working quiz

--- app.js
+++ app.js
@@ -1,82 +1,67 @@
 // Create your initial state object
 var appState = {
-   questions: [
-   		{ 
-   			question: "question1",
-   			choices: ['bob','2', '3', '4'],
-   			correctAnswer: 	0,
-        giveFeedback: 0
-   		},
-      
-      { 
-        question: "question2",
-        choices: ['bob','2', '3', '4'],
-        correctAnswer: 0,
-        giveFeedback: 0
-      },
+  questions: [
+    { 
+      question: "Who is Pharah's mother?",
+      choices: ["Mercy","Ana","Torbjorn","Diva"],
+      correctAnswer: 1,
+      giveFeedback: 0
+    },
 
-      { 
-        question: "question3",
-        choices: ['bob','2', '3', '4'],
-        correctAnswer: 0,
-        giveFeedback: 0
-      },
-      
-      { 
-        question: "question4",
-        choices: ['bob','2', '3', '4'],
-        correctAnswer: 0,
-        giveFeedback: 0
-      },
-      
-      { 
-        question: "question5",
-        choices: ['bob','2', '3', '4'],
-        correctAnswer: 0,
-        giveFeedback: 0
-      }
+    { 
+      question: "Which of these characters is not a support?",
+      choices: ["Tracer","Lucio","Symmetra","Mercy"],
+      correctAnswer: 0,
+      giveFeedback: 0
+    },
 
-   ],
-   score: 0,
-   currentQuestionIndex: null,
-   answerArray: []
+    { 
+      question: "What is the name of Reinhardt's ultimate ability?",
+      choices: ["Rip-tire","Sound-barrier","Pulse Bomb","Earthshatter"],
+      correctAnswer: 3,
+      giveFeedback: 0
+    },
+
+    { 
+      question: "Who is the brother of Genji?",
+      choices: ["Soldier76","Hanzo","Roadhog","Bastion"],
+      correctAnswer: 1,
+      giveFeedback: 0
+    },
+
+    { 
+      question: "Which of these heroes has no soul?",
+      choices: ["Reaper","Mei","Junkrat","Zarya"],
+      correctAnswer: 1,
+      giveFeedback: 0
+    }
+
+    ],
+      score: 0,
+      currentQuestionIndex: null,
+      answerArray: []
 };
 
 
 // State manipulation functions...
 // function to check user answer (if statement display fb)
 function submitAnswer(state, userInput) {
-	const currentQuestion = state.questions[state.currentQuestionIndex];
-	
-	state.answerArray.push(userInput);
-  console.log(state.answerArray);
-  console.log('outside the if statement');
-  console.log(state.currentQuestionIndex);
-  console.log(state.answerArray[state.currentQuestionIndex]);
-  console.log(currentQuestion.correctAnswer);
-	
+  const currentQuestion = state.questions[state.currentQuestionIndex];
+  state.answerArray.push(userInput);
 }
 
-  function nextQuestion(state) {
-  	//incrementing currentQuestionIndex
-  	// if on last question, show results page after clicking next
-  	if(state.currentQuestionIndex === null) {
-  		state.currentQuestionIndex = 0;
-  		// return true;
-  	}
-   	// else if(state.currentQuestionIndex === state.questions.length-1) {
-  		// return false;
-  	//}
-  	else {
-  		state.currentQuestionIndex++;
-  		// return true;
-  	}
+function nextQuestion(state) {
+  if(state.currentQuestionIndex === null) {
+    state.currentQuestionIndex = 0;
+  }
+  else {
+    state.currentQuestionIndex++;
+  }
 }
 
 function getFeedback(state) {
   const currentQuestion = state.questions[state.currentQuestionIndex];
   if (state.answerArray[state.currentQuestionIndex] === currentQuestion.correctAnswer) {
-    console.log("Inside the If statement");
     currentQuestion.giveFeedback = 1;
     state.score++;
   } else {
@@ -85,84 +70,80 @@ function getFeedback(state) {
 }
 
 function loadQuestion(state, element) {
-// call nextquestion
-// make sure question was answered before
-// call submitanswer --> if statement gives fb + score, which put into html
-// // if last question results html ?
-// forEach
-// let quizHTML = state.questions.map(function(question) {
+
   let questionIndex = state.questions[state.currentQuestionIndex];
   let quizHTML;
   if(state.currentQuestionIndex === null) {
-    quizHTML = `<h1>Quiz</h1>
-    <p>Let's take a quiz!</p>
+    quizHTML = `<h1>Overwatch Quiz</h1>
+    <p>This Quiz is about the game Overwatch, created by Blizzard Entertainment.
+    Please take as much time as you need to answer all of the questions.
+    When you are ready to begin, hit the start button. Good Luck!</p>
     <button class='start-button' type='button'>Start</button>`
   }
   else if(state.currentQuestionIndex < state.questions.length && questionIndex.giveFeedback === 0) {
-    quizHTML = `<form id='quiz-form'> 
+    quizHTML = `<section class="quiz"><form id='quiz-form'> 
     <p class='question'>${state.currentQuestionIndex+1}/${state.questions.length}: ${questionIndex.question}</p>
     <label class='option'><input type="radio" name="option" value='0'><span id='option0'>${questionIndex.choices[0]}</span></label>
     <label class='option'><input type="radio" name="option" value='1'><span id='option1'>${questionIndex.choices[1]}</span></label>
     <label class='option'><input type="radio" name="option" value='2'><span id='option2'>${questionIndex.choices[2]}</span></label>
     <label class='option'><input type="radio" name="option" value='3'><span id='option3'>${questionIndex.choices[3]}</span></label>
     </form>
-    <button class='submit-answer' type='button'>Submit answer</button>`
-    // <button class='hidden'>
+    <button class='submit-answer' type='button'>Submit answer</button>
+    </section>`
+
   }
   else if (state.currentQuestionIndex < state.questions.length && questionIndex.giveFeedback === 1) {
-    quizHTML = `<p>Correct! You have anwered ${state.score} out of ${state.questions.length} correct so far!</p>
-    <button class='next-question' type='button'>Next question</button>`
+    quizHTML = `<section class="quiz"><p class='feedback'>Correct! You have anwered ${state.score} out of ${state.questions.length} correct so far!</p>
+    <button class='next-question' type='button'>Next question</button>
+    </section>`
   }
   else if (state.currentQuestionIndex < state.questions.length && questionIndex.giveFeedback === 2) {
-    quizHTML = `<p>Wrong! The correct answer is ${questionIndex.choices[questionIndex.correctAnswer]}. You have anwered ${state.score} out of ${state.questions.length} correct so far!</p>
-    <button class='next-question' type='button'>Next question</button>`
+    quizHTML = `<section class="quiz"><p class='feedback'>Wrong! The correct answer is ${questionIndex.choices[questionIndex.correctAnswer]}. You have anwered ${state.score} out of ${state.questions.length} correct so far!</p>
+    <button class='next-question' type='button'>Next question</button>
+    </section>`
   }
   else {
-    quizHTML = `<section class='results'>
+    quizHTML = `<section class="quiz">
     <p class ='your-score'>You scored ${state.score} out of ${state.questions.length} correct!</p>
     <button class='start-over' type='button'>Start Over</button>
     </section>`
   }
 
-  $(element).html(quizHTML);
+    $(element).html(quizHTML);
 }
 
 
-//non skippable
-// Event handlers
 $(function(){
-  // When start button is submitted
+
   loadQuestion(appState, $('.main-container'));
 
+  // When start button is submitted
   $('.main-container').on('click', '.start-button',function(event) {
     nextQuestion(appState);
     loadQuestion(appState, $('.main-container'));  
   });
-
   // Current answer is submitted
   $('.main-container').on('click','.submit-answer',function(event) {
     var selectedOption = $('input[type=radio]:checked');
     var answer = parseInt(selectedOption.val());
-    // console.log(`Answer: ${answer}`);
+  if(selectedOption.length === 0) {
+    $('.error-text').text("Please select an answer!!!");
+  }
+  else {
     submitAnswer(appState, answer);
     getFeedback(appState);
     loadQuestion(appState, $('.main-container'));
-    //nextQuestion(appState);
-    
-    // console.log(` AnswerArray = ${appState.answerArray}`);
-    // console.log(` Feedback = ${appState.questions[0].giveFeedback}`);
-  });
+    $('.error-text').text("");
+  }
+});
   // next question
   $('.main-container').on('click','.next-question',function(event) {
     var selectedOption = $('input[type=radio]:checked');
     var answer = parseInt(selectedOption.val()); 
-    //getFeedback(appState);
+
     nextQuestion(appState);
     loadQuestion(appState, $('.main-container'));
-    
-    //submitAnswer(appState, answer);
   });
-
   // Restart button is clicked
   $('.main-container').on('click', '.start-over',function(event) {
     appState.currentQuestionIndex = null;
@@ -172,9 +153,9 @@ $(function(){
     appState.questions[1].giveFeedback = 0;
     appState.questions[2].giveFeedback = 0;
     appState.questions[3].giveFeedback = 0;
-    
+
     loadQuestion(appState, $('.main-container'));  
     nextQuestion(appState);
     loadQuestion(appState, $('.main-container'));  
-  });
+    });
 });
